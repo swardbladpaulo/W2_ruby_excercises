59680e0b2bbbee0d866249007268c36bf3f69adc
swardbladpaulo <paulo@swardblads.se>
2020-10-10 19:15:27 +0200

    created my_group.rb, created 3 hashes, person_1, person_2 and person_3, stored this hashes in an array my_group

---
+++ my_group.rb
@@ -0,0 +1,6 @@
+person_1 = {name: "person_1", gender: "male", age: 25}
+person_2 = {name: "person_2", gender: "female", age: 21}
+person_3 = {name: "person_3", gender: "hen", age: 65}
+
+my_group = Array.new
+my_group = ["person_1", "person_2", "person_3"]
